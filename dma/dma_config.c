31977abf85cbb0f96184d9b6972641dbe022a267
Romanch <romanchneupane53@gmail.com>
2025-10-30 23:25:10 +0545

    added dma

---
+++ dma/dma_config.c
@@ -0,0 +1,84 @@
+#include "stm32f1xx.h"
+#include "usart_driver.h"
+uint8_t data_buffer[5] = {10,20,30,40,50};
+
+USART_Config_t USART_Config = {
+	.Instance    = USART2,
+	.BaudRate    = 9600,
+	.WordLength  = 8,
+	.StopBits    = 1,
+	.Parity      = 0    // 0 = none
+};
+void main(){
+	usart_init(&USART_Config);
+	dma_config();
+}
+
+
+//dma configuration
+void dma_config(){
+    RCC -> AHBENR |= RCC_AHBENR_DMA1EN; //enable clock for dma1
+//-----------------------------------------------------------------//
+//-->>>    //EACH CHANNEL HAS ITS OWN SET OF REGISTERS        <<<--//
+//-----------------------------------------------------------------//
+    //channel configuration register - usart is at channel 7 so channel 7 registers should be configured
+    DMA1_Channel7 -> CCR |= DMA_CCR_MINC; //memory increment mode
+    DMA1_Channel7 -> CCR |= DMA_CCR_DIR; //read from memory and write to peripheral
+    DMA1_Channel7 -> CCR |= DMA_CCR_TEIE //transfer error interrupt enable
+                            | DMA_CCR_TCIE  //transfer complete interrupt enable
+                            | DMA_CCR_HTIE; //half transfer interrupt enable
+
+
+    //number of data register
+    uint16_t data_length = 5; //number of data to be transferred
+    //after enabling the channel, this register is read only and decremented after each data transfer
+    DMA1_Channel7 -> CNDTR = data_length;
+
+    //peripheral address register
+    uint32_t peripheral_address = (uint32_t)(&(USART2 -> DR)); //or (uint32_t)((*usart2).DR) or 0x40004404 directly
+    DMA1_Channel7 -> CPAR = peripheral_address;
+
+    //memory address register
+    uint32_t *memory_address = &data_buffer[0]; // or *memory address = data_buffer; instead
+    DMA1_Channel7 -> CMAR = (uint32_t)memory_address;
+
+
+    NVIC_EnableIRQ(DMA1_Channel7_IRQn); //enable dma1 channel1 interrupt in nvic
+
+    DMA1_Channel7 -> CCR |= DMA_CCR_EN; //enable dma channel
+}
+
+//dma1 channel1 interrupt handler
+
+void DMA1_Channel7_IRQHandler(void) {
+    // Transfer Complete
+    if (DMA1->ISR & DMA_ISR_TCIF7) {
+        DMA1->IFCR |= DMA_IFCR_CTCIF7;   // clear flag
+
+        // Brief blink to indicate success
+        GPIOC->BSRR = GPIO_BSRR_BR13;    // LED ON (active-low)
+        for (volatile int i = 0; i < 1000000; i++);
+        GPIOC->BSRR = GPIO_BSRR_BS13;    // LED OFF
+    }
+
+    // Half Transfer
+    if (DMA1->ISR & DMA_ISR_HTIF7) {
+        DMA1->IFCR |= DMA_IFCR_CHTIF7;   // clear flag
+
+        // Quick toggle to indicate half transfer
+        //GPIOC->ODR ^= GPIO_ODR_ODR13;
+    }
+
+    // Transfer Error
+    if (DMA1->ISR & DMA_ISR_TEIF7) {
+        DMA1->IFCR |= DMA_IFCR_CTEIF7;   // clear flag
+
+        // Fast blinking to indicate error (6 blinks)
+        for (int i = 0; i < 6; i++) {
+            GPIOC->ODR ^= GPIO_ODR_ODR13;
+            for (volatile int d = 0; d < 80000; d++);
+        }
+        GPIOC->BSRR = GPIO_BSRR_BS13;  // Ensure LED is OFF at end
+    }
+}
+
