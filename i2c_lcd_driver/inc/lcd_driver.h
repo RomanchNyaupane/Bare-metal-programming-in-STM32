8efd159bc5739ec2b609986f0e7313e2a3dabcae
Romanch <romanchneupane53@gmail.com>
2025-11-23 22:42:52 +0545

    added lcd driver header file

--- i2c_lcd_driver/inc/lcd_driver.h
+++ i2c_lcd_driver/inc/lcd_driver.h
@@ -6,8 +6,15 @@
 typedef I2C_Config_t LCD_Config_t;
 
 void lcd_init();
+void lcd_deinit();
 void lcd_print_string(char *);
 void lcd_end();
+void lcd_set_cursor_position(uint8_t row, uint8_t col);
+void lcd_clear_screen();
+void lcd_reset_cursor_position();
+void lcd_set_contrast(uint8_t);
+void lcd_shift_display_left(uint8_t);
+void lcd_shift_display_right(uint8_t);
 
 extern LCD_Config_t lcd_config;
 #endif /* LCD_DRIVER_H_ */
